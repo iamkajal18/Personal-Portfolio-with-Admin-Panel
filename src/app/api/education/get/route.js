0b1c114427873e4a53f99115a37d6bd055880062
iamkajal18 <152779714+iamkajal18@users.noreply.github.com>
2025-08-29 12:51:54 +0530

    refactor: simplify GET function by removing unused parameter and improving response messages

--- src/app/api/education/get/route.js
+++ src/app/api/education/get/route.js
@@ -4,12 +4,12 @@ import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
-export async function GET(req) {
+export async function GET() {
   try {
     await connectToDB();
     const extractData = await Education.find({});
 
-    if (extractData) {
+    if (extractData.length > 0) {
       return NextResponse.json({
         success: true,
         data: extractData,
@@ -17,15 +17,15 @@ export async function GET(req) {
     } else {
       return NextResponse.json({
         success: false,
-        message: "Something went wrong !Please try again",
+        message: "No education data found",
       });
     }
   } catch (e) {
-    console.log(e);
-
+    console.error("Error fetching Education data:", e);
     return NextResponse.json({
       success: false,
-      message: "Something went wrong !Please try again",
+      message: "Failed to fetch education data. Please try again later.",
+      error: e.message,
     });
   }
 }
